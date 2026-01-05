fd232def8bfa09b9021396fc5db215216a3464b2
Huyntrangxinh <lahuyentrang25678@gmail.com>
2026-01-05 09:40:55 +0700

    Refine segmented control UI (active/inactive states)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,8 +2,57 @@
 
 public partial class MainPage : ContentPage
 {
+    private enum SegmentOption { Compact, Smart, Premium }
+    private SegmentOption _selectedOption = SegmentOption.Compact;
+
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void UpdateSegmentControl(SegmentOption selected)
+    {
+        _selectedOption = selected;
+
+        // Hide all frames and show all labels
+        CompactFrame.IsVisible = false;
+        SmartFrame.IsVisible = false;
+        PremiumFrame.IsVisible = false;
+
+        CompactLabel.IsVisible = true;
+        SmartLabel.IsVisible = true;
+        PremiumLabel.IsVisible = true;
+
+        // Set active state - show frame and hide label
+        switch (selected)
+        {
+            case SegmentOption.Compact:
+                CompactFrame.IsVisible = true;
+                CompactLabel.IsVisible = false;
+                break;
+            case SegmentOption.Smart:
+                SmartFrame.IsVisible = true;
+                SmartLabel.IsVisible = false;
+                break;
+            case SegmentOption.Premium:
+                PremiumFrame.IsVisible = true;
+                PremiumLabel.IsVisible = false;
+                break;
+        }
+    }
+
+    private void OnSmartTapped(object sender, EventArgs e)
+    {
+        UpdateSegmentControl(SegmentOption.Smart);
+    }
+
+    private void OnPremiumTapped(object sender, EventArgs e)
+    {
+        UpdateSegmentControl(SegmentOption.Premium);
+    }
+
+    private void OnCompactTapped(object sender, EventArgs e)
+    {
+        UpdateSegmentControl(SegmentOption.Compact);
     }
 }
