c5753ef3eb46a36ca775ad13fbbff32ea803c4e2
Huyntrangxinh <lahuyentrang25678@gmail.com>
2026-01-05 08:19:16 +0700

    Add SmartHome MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartHomeDashboard2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHomeDashboard2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
